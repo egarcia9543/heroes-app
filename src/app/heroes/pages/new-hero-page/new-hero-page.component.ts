9e9f685faad95a4423ac95f0585e4fca40a4b6f4
esteban.garcia2 <esteban.garcia@pragma.com.co>
2024-05-26 17:49:49 -0500

    chore: Update HeroesService with CRUD operations for heroes

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-hero-page',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrl: './new-hero-page.component.css'
 })
 export class NewHeroPageComponent {
-  public publishers = ['Marvel', 'DC Comics'];
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' }
+  ];
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_image: new FormControl(''),
+  });
+
+  constructor(private heroesSrv: HeroesService) { };
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit() {
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroesSrv.updateHero(this.currentHero)
+        .subscribe(hero => {
+          console.log(hero)
+        })
+        return;
+      }
+
+    this.heroesSrv.addHero(this.currentHero)
+      .subscribe(hero => {
+        console.log(hero)
+      })
+  }
+
+
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interfaces';
 import { environments } from '../../../environments/environments';
 
@@ -21,5 +21,21 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=4`)
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Id requerido')
+    return this.httpClient.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError(err => of(false)),
+      map(resp => true)
+    )
   }
 }
