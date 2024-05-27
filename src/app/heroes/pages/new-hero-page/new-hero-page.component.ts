00eb8acbb487e3bd6a8959ff88f4f29d08ebe61a
esteban.garcia2 <esteban.garcia@pragma.com.co>
2024-05-26 23:34:32 -0500

    feat: Update hero card layout and add lazy loading for hero images

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interfaces';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-hero-page',
   templateUrl: './new-hero-page.component.html',
   styleUrl: './new-hero-page.component.css'
 })
-export class NewHeroPageComponent {
+export class NewHeroPageComponent implements OnInit {
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' }
   ];
+  public isEditing: boolean = false;
 
   public heroForm = new FormGroup({
     id: new FormControl(''),
@@ -24,7 +27,35 @@ export class NewHeroPageComponent {
     alt_image: new FormControl(''),
   });
 
-  constructor(private heroesSrv: HeroesService) { };
+  constructor(private heroesSrv: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { };
+
+  ngOnInit(): void {
+    if (this.router.url.includes('edit')) {
+      this.isEditing = true;
+      const id = this.activatedRoute.snapshot.paramMap.get('id');
+      if (id) {
+        this.heroesSrv.getHeroById(id).subscribe(hero => {
+          if (!hero) {
+            return this.router.navigateByUrl('/');
+          }
+          this.heroForm.patchValue({
+            id: hero?.id,
+            superhero: hero?.superhero,
+            publisher: hero?.publisher,
+            alter_ego: hero?.alter_ego,
+            first_appearance: hero?.first_appearance,
+            characters: hero?.characters,
+            alt_image: hero?.alt_image
+          });
+          return;
+        })
+      }
+    }
+  }
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
@@ -37,16 +68,21 @@ export class NewHeroPageComponent {
     if (this.currentHero.id) {
       this.heroesSrv.updateHero(this.currentHero)
         .subscribe(hero => {
-          console.log(hero)
+          this.showSnackbar(`Excelente, ${hero.superhero} ha sido actualizado correctamente`)
         })
-        return;
-      }
+      return;
+    }
 
     this.heroesSrv.addHero(this.currentHero)
       .subscribe(hero => {
-        console.log(hero)
+        this.showSnackbar(`Excelente, ${hero.superhero} ha sido agregado correctamente`);
+        this.router.navigateByUrl(`/heroes/edit/${hero.id}`);
       })
   }
 
-
+  showSnackbar(message: string): void {
+    this.snackBar.open(message, 'Listo', {
+      duration: 3000
+    });
+  }
 }
